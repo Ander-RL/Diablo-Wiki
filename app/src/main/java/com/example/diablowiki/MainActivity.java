419a0cc9ad4df1131e2521ced41f8cb8616878f2
Ander-RL <anderrodriguez205@gmail.com>
2020-09-18 13:04:25 +0200

    Added Drawer Menu

--- app/src/main/java/com/example/diablowiki/MainActivity.java
+++ app/src/main/java/com/example/diablowiki/MainActivity.java
@@ -1,18 +1,80 @@
 package com.example.diablowiki;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer_main);
         //Setting the toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        if (drawer != null) {
+            drawer.addDrawerListener(toggle);
+        }
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(MainActivity.this);
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    /**
+     * Handles a navigation drawer item click. It detects which item was
+     * clicked and displays a toast message showing which item.
+     * @param item  Item in the navigation drawer
+     * @return      Returns true after closing the nav drawer
+     */
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        // Handle navigation view item clicks here.
+        switch (item.getItemId()) {
+            case R.id.nav_about_game:
+                drawer.closeDrawer(GravityCompat.START);
+                displayToast(getString(R.string.place_holder));
+                return true;
+            case R.id.nav_lore:
+                drawer.closeDrawer(GravityCompat.START);
+                displayToast(getString(R.string.place_holder));
+                return true;
+            case R.id.nav_characters:
+                drawer.closeDrawer(GravityCompat.START);
+                displayToast(getString(R.string.place_holder));
+                return true;
+            case R.id.nav_items:
+                drawer.closeDrawer(GravityCompat.START);
+                displayToast(getString(R.string.place_holder));
+                return true;
+            case R.id.nav_talents:
+                drawer.closeDrawer(GravityCompat.START);
+                displayToast(getString(R.string.place_holder));
+                return true;
+            default:
+                return false;
+        }
     }
 }
