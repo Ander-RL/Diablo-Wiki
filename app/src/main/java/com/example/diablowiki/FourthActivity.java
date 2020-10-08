d8213fc0f9d8d2f7527ce68abb4ff93b05319026
Ander-RL <anderrodriguez205@gmail.com>
2020-10-08 19:51:33 +0200

    Item Activity filter added. TODO set filter initial status to true and persist.

--- app/src/main/java/com/example/diablowiki/FourthActivity.java
+++ app/src/main/java/com/example/diablowiki/FourthActivity.java
@@ -7,11 +7,13 @@ import android.content.res.TypedArray;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -24,13 +26,16 @@ public class FourthActivity extends AppCompatActivity {
     String[] weapon, damage, durability, requirements, price, quality;
     TypedArray imagesArray;
     int[] images;
-    ArrayList selectedItems;
-
+    private ArrayList<Integer> selectedItems = new ArrayList<>(2);
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fourth);
+
+        if (savedInstanceState != null) {
+            selectedItems.addAll(savedInstanceState.getIntegerArrayList("selectedItems"));
+        }
 
         getIntent();
 
@@ -72,7 +77,8 @@ public class FourthActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        FourthActivityDialogFragment dialog = new FourthActivityDialogFragment();
+        FourthActivityDialogFragment dialog = new FourthActivityDialogFragment(this);
+        //if(selectedItems != null){ dialog.setmSelectedItems(selectedItems);}
         dialog.show(getSupportFragmentManager(), "Alert Dialog Fragment");
         return super.onOptionsItemSelected(item);
     }
@@ -80,5 +86,17 @@ public class FourthActivity extends AppCompatActivity {
     @Override
     protected void onSaveInstanceState(@NonNull Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putIntegerArrayList("selectedItems", getSelectedItems());
     }
+
+    public void setSelectedItems(ArrayList<Integer> selectedItems) {
+        //this.selectedItems = (ArrayList<Integer>)selectedItems.clone();
+        //this.selectedItems.addAll(selectedItems);
+        this.selectedItems = selectedItems;
+    }
+
+    public ArrayList<Integer> getSelectedItems() {
+        return this.selectedItems;
+    }
+
 }
--- app/src/main/java/com/example/diablowiki/FourthActivityDialogFragment.java
+++ app/src/main/java/com/example/diablowiki/FourthActivityDialogFragment.java
@@ -6,28 +6,83 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
+
+import com.example.diablowiki.FourthActivity;
+import com.example.diablowiki.R;
+
+import java.util.ArrayList;
 
 public class FourthActivityDialogFragment extends DialogFragment {
 
+    private ArrayList<Integer> mSelectedItems;
+    boolean[] choices = new boolean[]{false, false};
+    FourthActivity filter;
+
+    public FourthActivityDialogFragment(FourthActivity filter){
+        this.filter = filter;
+        this.mSelectedItems = filter.getSelectedItems();
+    }
+
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Where we track the selected items
+        //mSelectedItems = new ArrayList<>();
+        //filter = new FourthActivity();
+        //if(savedInstanceState != null){
+        if (mSelectedItems != null) {
+            for (int i = 0; i < mSelectedItems.size(); i++) {
+                if (mSelectedItems.contains(i)) {
+                    choices[i] = true;
+                }
+            }
+        }
+        //if (mSelectedItems.contains(0)){choices[0] = true;}
+        //if (mSelectedItems.contains(1)){choices[1] = true;}
+        //if (mSelectedItems.contains(0) && mSelectedItems.contains(1)){choices[0] = true; choices[1] = true;}
+        //}
         // Use the Builder class for convenient dialog construction
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        builder.setMessage("Mensaje")
-                .setPositiveButton(R.string.menu_filter_dialog_ok, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        Toast toast = new Toast(getContext());
-                        toast.makeText(getContext(), "Place Holder", Toast.LENGTH_SHORT).show();
+        builder.setTitle("Titulo");
+        // Specify the list array, the items to be selected by default (null for none),
+        // and the listener through which to receive callbacks when items are selected
+        builder.setMultiChoiceItems(R.array.filter_options, choices, new DialogInterface.OnMultiChoiceClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which, boolean isChecked) {
+                if (isChecked) {
+                    if (!mSelectedItems.contains(which)) {
+                        mSelectedItems.add(which);
                     }
-                })
+                }
+                if (!isChecked){
+                    if (mSelectedItems.contains(which)) {  //TODO DEBUGEAR
+                        if(mSelectedItems.size() == 1){mSelectedItems = new ArrayList<>(2);} //TODO cambiar choices a true inicial y guardar persistencia
+                        else {mSelectedItems.remove(which);}
+                    }
+                }
+            }
+        });
+        builder.setPositiveButton(R.string.menu_filter_dialog_ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                filter.setSelectedItems(mSelectedItems);
+            }
+        })
                 .setNegativeButton(R.string.menu_filter_dialog_cancel, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
-                        Toast toast = new Toast(getContext());
-                        toast.makeText(getContext(), "Place Holder", Toast.LENGTH_SHORT).show();
+                        filter.setSelectedItems(new ArrayList<Integer>(2));
                     }
                 });
         // Create the AlertDialog object and return it
         return builder.create();
+    }
+
+    public ArrayList<Integer> getmSelectedItems() {
+        return mSelectedItems;
+    }
+
+    public void setmSelectedItems(ArrayList<Integer> mSelectedItems) {
+        this.mSelectedItems = mSelectedItems;
     }
 }
